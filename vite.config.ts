d9f5dd1f7b2c51885ec94f5a5f265f7969e19e12
Artem Anzin <anzinartix@gmail.com>
2023-09-21 22:55:18 +0300

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    emptyOutDir: true,
+  },
+  server: {
+    open: true,
+  },
+  preview: {
+    open: true,
+  },
+  resolve: {
+    alias: {
+      "@": resolve("src"),
+    },
+  },
+});
